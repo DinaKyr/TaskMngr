d86d9340808b8d5f9a793e125a15acb2b5c8e88f
DinaKyr <kwnstantinakyri@gmail.com>
2026-01-20 12:48:52 +0200

    Add EmailService

---
+++ src/main/java/com/example/TaskMngr/services/EmailService.java
@@ -0,0 +1,28 @@
+package com.example.TaskMngr.services;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendInviteEmail(String toEmail, String inviteLink) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("You're invited to join TaskMngr");
+        message.setText(
+            "You have been invited to join TaskMngr.\n\n" +
+            "Click the link below to register:\n" +
+            inviteLink + "\n\n" +
+            "This link will expire."
+        );
+
+        mailSender.send(message);
+    }
+}
